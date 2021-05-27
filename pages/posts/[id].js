ba3f6c156c37a822a027271981816f179550d48e
sunny nguyen-bui <snguyenbui@gmail.com>
2021-05-27 12:51:19 -0700

    added getStaticPaths for dynamic routes

---
+++ pages/posts/[id].js
@@ -0,0 +1,14 @@
+import { getAllPostIds } from "../../lib/posts";
+import Layout from "../../components/layout";
+
+export default function Post() {
+  return <Layout>...</Layout>;
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}

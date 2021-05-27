19450ab7ac2dd4b3d4b1a277803e3808c606d9d2
sunny nguyen-bui <snguyenbui@gmail.com>
2021-05-27 14:00:16 -0700

    implemented getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,5 @@
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 import Layout from "../../components/layout";
-
-export default function Post() {
-  return <Layout>...</Layout>;
-}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -11,4 +7,25 @@ export async function getStaticPaths() {
     paths,
     fallback: false,
   };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
